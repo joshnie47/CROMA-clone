fd792bfc1b2217ceb009fed12ee8bc77b6db2c49
joshnie47 <22bcs047@psgrkcw.ac.in>
2025-09-24 22:12:53 +0530

    solving

--- Assets/scripts.js
+++ Assets/scripts.js
@@ -1,23 +1,16 @@
-// Get elements
 const menuBtn = document.getElementById("menubtn");
 const closeBtn = document.getElementById("closebtn");
 const navTab = document.querySelector(".nav-tab");
-
-// Initially hide close button & nav
 closeBtn.style.display = "none";
 navTab.style.display = "none";
-
-// When clicking the menu button
 menuBtn.addEventListener("click", () => {
-  menuBtn.style.display = "none";      // hide menu icon
-  closeBtn.style.display = "block";    // show close icon
-  navTab.style.display = "block";      // show menu list
+  menuBtn.style.display = "none";      
+  closeBtn.style.display = "block";    
+  navTab.style.display = "block";     
 });
-
-// When clicking the close button
 closeBtn.addEventListener("click", () => {
-  closeBtn.style.display = "none";     // hide close icon
-  menuBtn.style.display = "block";     // show menu icon
-  navTab.style.display = "none";       // hide menu list
+  closeBtn.style.display = "none";    
+  menuBtn.style.display = "block";   
+  navTab.style.display = "none";      
 });
 
